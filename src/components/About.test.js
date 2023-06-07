64a4f7a369f5f509279e73a0b6f0946f4b667adb
Witold Weiner <42048512+witke16@users.noreply.github.com>
2023-06-07 23:35:20 +0200

    Update About.test.js

--- src/components/About.test.js
+++ src/components/About.test.js
@@ -7,17 +7,15 @@ describe('About Component', () => {
     render(<About />);
   });
 
-  it('displays the correct text and image', () => {
+  it('displays the correct text', () => {
     const { getByText, getByAltText } = render(<About />);
     const headingElement = getByText('About');
     const textElement1 = getByText('This is a final project for the subject "Tworzenie nowoczesnych aplikacji frontendowych w praktyce".');
     const textElement2 = getByText('The website was created by Witold Weiner');
-    const imageElement = getByAltText('ToDoList helper');
 
     expect(headingElement).toBeInTheDocument();
     expect(textElement1).toBeInTheDocument();
     expect(textElement2).toBeInTheDocument();
-    expect(imageElement).toBeInTheDocument();
-    expect(imageElement.src).toContain('helper'); // assuming 'helper' is the expected image file name
+
   });
 });
